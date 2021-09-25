4ed0572f601cc5bc16092e38884f44c0d459134b
guilhermealvino <guilhermealvi9@gmail.com>
2021-09-25 19:38:26 -0300

    implementando alteracao e exclusao de pessoas

--- src/main/java/br/com/crud/crudproject/rh/controle/PessoaControle.java
+++ src/main/java/br/com/crud/crudproject/rh/controle/PessoaControle.java
@@ -1,9 +1,15 @@
 package br.com.crud.crudproject.rh.controle;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import br.com.crud.crudproject.rh.dominio.Pessoa;
 import br.com.crud.crudproject.rh.dominio.PessoaRepositorio;
 
 //Listar pessoas
@@ -23,5 +29,37 @@ public class PessoaControle {
 		return "rh/pessoas/index";
 	}
 	
+	@GetMapping("/rh/pessoas/nova")
+	public String novaPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
+		return "rh/pessoas/form";
+	}
+	
+	@GetMapping("/rh/pessoas/{id}")
+	public String alterarPessoa(@PathVariable("id") long id, Model model) {
+		Optional<Pessoa> pessoaOpt = pessoaRepo.findById(id);
+		if(pessoaOpt.isEmpty()) {
+			throw new IllegalArgumentException("Pessoa invalida");
+		}		
+		
+		model.addAttribute("pessoa", pessoaOpt.get());
+		return "rh/pessoas/form";
+	}
+	
+	@PostMapping("/rh/pessoas/salvar")
+	public String salvarPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
+		pessoaRepo.save(pessoa);
+		return "redirect:/rh/pessoas";
+	}
+	
+	@GetMapping("/rh/pessoas/excluir/{id}")
+	public String excluirPessoa(@PathVariable("id") Long id) {
+		Optional<Pessoa> pessoaOpt = pessoaRepo.findById(id);
+		if(pessoaOpt.isEmpty()) {
+			throw new IllegalArgumentException("Pessoa invalida");			
+		}
+		pessoaRepo.delete(pessoaOpt.get());
+		return "redirect:/rh/pessoas";
+		
+	}
 
 }
