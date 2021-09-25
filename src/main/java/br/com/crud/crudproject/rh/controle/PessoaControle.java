ad498643274c41d60ad51b49149bbeeb835dd123
guilhermealvino <guilhermealvi9@gmail.com>
2021-09-25 05:44:10 -0300

    Implementacao da listagem de pessoas

---
+++ src/main/java/br/com/crud/crudproject/rh/controle/PessoaControle.java
@@ -0,0 +1,27 @@
+package br.com.crud.crudproject.rh.controle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.crud.crudproject.rh.dominio.PessoaRepositorio;
+
+//Listar pessoas
+
+@Controller
+public class PessoaControle {
+	
+	private PessoaRepositorio pessoaRepo;
+	
+	public PessoaControle(PessoaRepositorio pessoaRepo) {
+		this.pessoaRepo = pessoaRepo;
+	}
+	
+	@GetMapping("/rh/pessoas")
+	public String pessoas(Model model) {
+		model.addAttribute("listaPessoas", pessoaRepo.findAll());
+		return "rh/pessoas/index";
+	}
+	
+
+}
